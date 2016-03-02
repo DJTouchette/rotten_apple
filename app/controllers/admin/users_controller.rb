7dac2153960d669f654ae63757ed48d9340048d4
DJTouchette <djtouchette1993@gmail.com>
2016-03-02 02:53:26 +0000

    Added hidden form field. False for user and true for admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,17 @@
 class Admin::UsersController < ApplicationController
   
-  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
 
 end
