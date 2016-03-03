168512170af706886400e36069866d6e8f88ab24
DJTouchette <djtouchette1993@gmail.com>
2016-03-03 19:50:56 +0000

    Added rotten apples instead of ratings

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,5 +3,7 @@ module MoviesHelper
   def formatted_date(date)
     date.strftime("%b %d, %Y")
   end
+
+
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+img = File.open(Dir['app/assets/images/*.jpg'].sample)
+
+  20.times do
+    User.create(
+      email: Faker::Internet.email,
+      password: 'password',
+      firstname: Faker::Name.name,
+      lastname: Faker::Name.name,
+      admin: Faker::Boolean.boolean(0.5)
+    )
+  end
+
+  20.times do
+    img = File.open(Dir['app/assets/images/*.jpg'].sample)
+    Movie.create(
+      title: Faker::Book.title,
+      description: Faker::Lorem.sentence(20),
+      image: img,
+      director: Faker::StarWars.character,
+      runtime_in_minutes: Faker::Number.between(50, 96),
+      release_date: Faker::Date.backward(14)
+    )
+  end
+
+  user_count = User.count
+  movie_count = Movie.count
+
+  20.times do
+    user_offset = rand(user_count)
+    movie_offset = rand(movie_count)
+    Review.create(
+      text: Faker::Lorem.sentence(3),
+      user_id: User.offset(user_offset).first,
+      movie_id: Movie.offset(movie_offset).first,
+      rating_out_of_ten: Faker::Number.between(1, 10)
+    )
+  end
+
+
+
