dd204ad4abef04489dd675add8b45757fb4ae8b4
DJTouchette <djtouchette1993@gmail.com>
2016-03-05 01:32:08 +0000

    Fixed search bar

---
+++ app/assets/images/RottenApples_files/movies.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function(){
+ 
+//  $.ajax({
+//      type: "POST",
+//      contentType: "application/json; charset=utf-8",
+//      url: "/movies/",
+//      data : JSON,
+//      dataType: "json",
+//      success: function (result) {
+      
+//         window.alert("success!!");
+//      },
+//      error: function (){
+//         window.alert("something wrong!");
+//      }
+// });
+      
+});
