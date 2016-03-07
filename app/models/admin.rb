0029a36f90083a264873c9de4d270309fb647111
Maher Manoubi <maher.manoubi@gmail.com>
2016-03-07 13:40:21 -0800

    Update admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,5 @@
+# MM5: I don't think you need to do this. What you are looking to do is add an admin role to the user instead of using inheritence
+# I don't see this class being used anywhere in your code.
 class Admin < User
 
 end
